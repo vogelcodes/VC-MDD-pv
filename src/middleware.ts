e3a699ac3d29f282f0e679230b6fd6f265abd04b
Daniel Vogel <54858106+vogelcodes@users.noreply.github.com>
2025-04-23 05:31:21 -0300

    gsheetsScripts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,29 +4,10 @@ const ADMIN_USERNAME = import.meta.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = import.meta.env.ADMIN_PASSWORD;
 
 export const onRequest = async (
-  context: { request: Request },
+  { locals, request }: { locals: any; request: Request },
   next: () => Promise<Response>
 ) => {
   console.time("onRequest");
-  const { request } = context;
-  if (request.url.includes("fbclid=")) {
-    //Get fbclid from url
-    const fbclid = request.url.split("fbclid=")[1];
-    const metaEventData = {
-      data: [
-        {
-          event_name: "ViewContent", // Standard event name for a lead
-          event_time: Math.floor(Date.now() / 1000), // Current timestamp in seconds
-          action_source: "website",
-          user_data: {
-            fbc: fbclid,
-          },
-        },
-      ],
-      test_event_code: "TEST41321",
-    };
-    sendToMeta(metaEventData);
-  }
   // Check if the request is for an admin route
   if (request.url.includes("/admin")) {
     const authHeader = request.headers.get("Authorization");
