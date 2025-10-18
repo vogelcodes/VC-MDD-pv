7e595097a2e3da76b0b944d5810e275bce8ffe52
Daniel Vogel <danielcvt@gmail.com>
2025-10-18 16:14:29 -0300

    vvv-v8

--- src/components/yt-player.tsx
+++ src/components/yt-player.tsx
@@ -4,12 +4,10 @@ import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 export default function YoutubePlayer() {
   return (
     <div
-      data-umami-event="Watch Video"
-      className="mx-auto mb-4 aspect-video w-full lg:max-w-[53.25rem]"
+            className="mx-auto mb-4 aspect-video w-full lg:max-w-[53.25rem]"
     >
       <LiteYouTubeEmbed
-        data-umami-event="Watch Video"
-        id="eXp3-bclWPs"
+        id="ND5HhW72fWM"
         title="Método LactoFlow®"
         poster="maxresdefault"
         webp
